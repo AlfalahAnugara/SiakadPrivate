c34fd81d587d764da8cdc2a445190d5dbb3c6bb7
AlfalahAnugara <alfalah190299@gmail.com>
2019-03-29 08:58:01 +0700

    proses membuat editguru

--- application/views/admin/editguru.php
+++ application/views/admin/editguru.php
@@ -3,3 +3,16 @@
 <head>
 </head>
 <body id = "page-top">
+
+	<div id = "content-wrapper" >
+	<div class = "container-fluid">
+
+	<?php if ($this->session->flashdata('success')): ?>
+	<div class = "alert alert-success" role = "alert">
+		<?php echo $this->session->flashdata('success') ; ?>
+	<div>
+	<?php endif ; ?>
+
+	<div class = "card-body">
+
+<form action=""
