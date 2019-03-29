c53f221de4c6ce464228b3538dff0eb0181c400a
AlfalahAnugara <alfalah190299@gmail.com>
2019-03-29 09:49:52 +0700

    proses membuat list guru

--- application/views/admin/listguru.php
+++ application/views/admin/listguru.php
@@ -4,23 +4,6 @@
 	
 </head>
 <body id = "page-top">
-
-	<?php $this->load->view("guru/_partials/navbar.php") ?>
-	<div id = "wrapper">
-
-		<?php $this->load->view("admin/_partials/sidebar.php") ?>
-		<div id = "content-wrapper">
-
-			<div class = "container-fluid">
-
-			<?php $this->load->view("admin/_partials/breadcrumb.php") ?>
-
-			<div class = "card mb-3">
-				<div class = "card-header">
-					<a href = "<?php echo site_url(). 'guru/add'?>"><i class = "fas fa-plus"></i>Add New</a>
-				</div>
-				<div class = "card-body">
-
 				<table width="100%">
 					<br><h2 align="center">Daftar Guru</h2>
 						<thead>
