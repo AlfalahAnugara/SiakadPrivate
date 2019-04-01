5227022f7aa6e3f1c83d586b3b42bf1715939797
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-01 20:40:46 +0700

    proses membuat crud admin

---
+++ application/models/Admin_model.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed') ;
+
+class Guru_model extends CI_Model {
+	private $_table = "pegawai" ;
+
+	
+	public $nip ;
+	public $nama ;
+	public $password ;
+	public $gender ;
+	public $status_user ; 
