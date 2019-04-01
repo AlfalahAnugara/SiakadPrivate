4e4c9776d3e23021bc2f54e155ff503ba8c29c06
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-01 20:48:13 +0700

    proses membuat crud pegawai

---
+++ application/views/admin/tambahpegawai.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body id = "page-top">
+
+		<div id = "content-wrapper" >
+
+			<div class = "container-fluid">
+
+			<?php if ($this->session->flashdata('success')): ?>
+			<div class = "alert alert-success" role = "alert">
+				<?php echo $this->session->flashdata('success') ; ?>
+			<div>
+			<?php endif ; ?>
+
+				<div class = "card-body">
