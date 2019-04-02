6790532ea18b5d32994140503d4794a9176cf5eb
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-02 15:11:50 +0700

    proses membuat edit pegawai

--- application/views/admin/editpegawai.php
+++ application/views/admin/editpegawai.php
@@ -13,6 +13,38 @@
 
 <input type = "hidden" name = "id" value = "<?php echo pegawai->id?>" />
 
+							<div class = "form-group">
+							<label for = "nip">nip*</label>
+							<input class = "form-control"
+							type = "text" name = "nip" placeholder = "nip baru" />			
+							</div>
 
+							<div class = "form-group">
+							<label for = "nama">Nama*</label>
+							<input class = "form-control"
+							type = "text" name = "nama" placeholder = "Nama baru" />			
+							</div>
+
+							<div class = "form-group">
+							<label for = "password">Password*</label>
+							<input class = "form-control"
+							type = "text" name = "password" placeholder = "Password baru" />			
+							</div>
+
+							<div class = "form-group">
+							<label for = "gender">Gender*</label>
+							<input class = "form-control"
+							type = "text" name = "gender" placeholder = "Gender baru" />			
+							</div>
+
+							<div class="form-group">
+            				<label for="Kelas">Status User:</label>
+                			<select class="form-control" name="status_user">
+                    		<option value="" disabled selected>Pilih User</option>
+							<option value="1">Siswa</option>
+							<option value="2">Guru</option>
+							<option value="3">Pegawai</option>
+							</select>
+							</div>
 
 
