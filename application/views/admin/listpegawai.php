e57baafc3ce39058666a5c390b560b166fded4bb
AlfalahAnugara <alfalah190299@gmail.com>
2019-04-02 14:45:38 +0700

    proses membuat list pegawai

--- application/views/admin/listpegawai.php
+++ application/views/admin/listpegawai.php
@@ -3,7 +3,29 @@
 <head>
 	
 </head>
-<body>
+<body id = "page-top">
+				<table width="100%">
+					<br><h2 align="center">Daftar Pegawai</h2>
+						<thead>
+						<tr align="center">
+							<th><font face ="Calibri">NIP</font></th>
+							<th><font face ="Calibri">Nama</font></th>
+							<th><font face ="Calibri">Password</font></th>
+							<th><font face ="Calibri">Jenis Kelamin</font></th>
+							<th><font face ="Calibri">Status User</font></th>
+
+							</tr>
+						<tbody>
+									<?php
+									foreach($pegawai as $admin) :
+									?>
+							<tr>
+
+							<td><p><?= $dosen->nig?></p></td>
+							<td><p><?= $dosen->nama?></p></td>
+							<td><p><?= $dosen->tgl_lahir?></p></td>
+										<td><p><?= $dosen->kota_asl?></p></td>
+										<td><p><?= $dosen->gender?></p></td>
 	
 </body>
 </html>
